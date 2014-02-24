f94d1f3daf4a485ba6953324895353e4111bf428
Hugo Mercier <hugo.mercier@oslandia.com>
2014-02-24 11:11:12 +0100

    Allow to save default settings

--- maindialog.py
+++ maindialog.py
@@ -1,5 +1,6 @@
 from PyQt4.QtCore import * 
 from PyQt4.QtGui import *
+from PyQt4.QtXml import *
 from qgis.core import *
 from qgis.gui import *
 
@@ -28,6 +29,10 @@ class MainDialog( QDialog ):
         self.ui.editStyleBtn.clicked.connect( self.on_style_edit )
         # connect file browser
         self.ui.browseBtn.clicked.connect( self.on_file_browse )
+        # add a "save as defaults" button
+        self.ui.saveDefaultsBtn = QPushButton( "Save as defaults", self.ui.buttonBox )
+        self.ui.buttonBox.addButton( self.ui.saveDefaultsBtn, QDialogButtonBox.ActionRole )
+        self.ui.saveDefaultsBtn.clicked.connect( self.on_save_defaults )
 
         # mask_mode: selection | mask
         self.mask_mode = None
@@ -40,18 +45,63 @@ class MainDialog( QDialog ):
         self.file_path = None
         self.file_format = None
 
-        self.update_style( self.layer )
-
         # init save format list
         for k,v in QgsVectorFileWriter.ogrDriverList().iteritems():
             self.ui.formatCombo.addItem( k )
 
+        # load defaults
+        self.load_defaults()
+
+        self.update_style( self.layer )
+
+    def load_defaults( self ):
+        settings = QSettings("AEAG", "QGIS Mask")
+
+        style = settings.value( "style", None )
+        if style:
+            doc = QDomDocument( "qgis" )
+            doc.setContent( style )
+            errorMsg = ''
+            self.layer.readSymbology( doc.firstChildElement("qgis"), errorMsg )
+
+        # restore othe defaults settings
+        self.ui.contentCombo.setCurrentIndex( int(settings.value( "mask_mode", 0 )) )
+        self.ui.bufferGroup.setChecked( settings.value("do_buffer", False) == "true" )
+        self.ui.saveLayerGroup.setChecked( settings.value("do_save_as", False) == "true" )
+        self.ui.bufferUnits.setText( str(settings.value("buffer_units", 1)) )
+        self.ui.bufferSegments.setText( str(settings.value("buffer_segments", 5)) )
+        self.ui.formatCombo.setCurrentIndex( int(settings.value( "file_format", 0 )) )
+
+    def on_save_defaults( self ):
+        doc = QDomDocument( QDomImplementation().createDocumentType( "qgis", "http://mrcc.com/qgis.dtd", "SYSTEM" ) )
+        rootNode = doc.createElement( "qgis" );
+        doc.appendChild( rootNode );
+        errorMsg = ''
+        self.layer.writeSymbology( rootNode, doc, errorMsg )
+
+        settings = QSettings("AEAG", "QGIS Mask")
+        settings.setValue( "style", doc.toByteArray() )
+        settings.setValue( "mask_mode", self.ui.contentCombo.currentIndex() )
+        settings.setValue( "do_buffer", self.ui.bufferGroup.isChecked() )
+        settings.setValue( "buffer_units", float(self.ui.bufferUnits.text() or 0) )
+        settings.setValue( "buffer_segments", float(self.ui.bufferSegments.text() or 0) )
+        settings.setValue( "do_save_as", self.ui.saveLayerGroup.isChecked() )
+        settings.setValue( "file_format", self.ui.formatCombo.currentIndex() )
+
     def on_file_browse( self ):
-        fn = QFileDialog.getSaveFileName( None, "Select a filename to save the mask layer to" )
+        settings = QSettings("AEAG", "QGIS Mask")
+
+        dir = settings.value("file_dir", '')
+
+        fn = QFileDialog.getSaveFileName( None, "Select a filename to save the mask layer to", dir )
         if not fn:
             return
 
         self.ui.filePath.setText( fn )
+
+        # save dir
+        fi = QFileInfo(fn)
+        settings.setValue("file_dir", fi.absolutePath())
 
     def on_style_edit( self ):
         # QgsRenderV2PropertiesDialog has a Cancel button that is not correctly plugged
@@ -103,10 +153,10 @@ class MainDialog( QDialog ):
         # get data before closing
         idx = self.ui.contentCombo.currentIndex()
         self.mask_mode = ('selection', 'mask')[idx]
-        self.do_buffer = self.ui.bufferGroup.isEnabled()
+        self.do_buffer = self.ui.bufferGroup.isChecked()
         self.buffer_units = float(self.ui.bufferUnits.text() or 0)
         self.buffer_segments = float(self.ui.bufferSegments.text() or 0)
-        self.do_simplify = self.ui.simplifyGroup.isEnabled()
+        self.do_simplify = self.ui.simplifyGroup.isChecked()
         self.simplify_tolerance = float(self.ui.simplifyTolerance.text() or 0)
 
         # get save as
