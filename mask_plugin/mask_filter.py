ddd9a0a3e70e6f03b456842d72be7e0066c4ea49
Hugo Mercier <hugo.mercier@oslandia.com>
2014-03-20 17:43:55 +0100

    Use data defined properties for labelling

--- mask_plugin/mask_filter.py
+++ mask_plugin/mask_filter.py
@@ -1,7 +1,6 @@
 from qgis.core import *
 
-SPATIAL_FILTER_BEGIN = "CASE WHEN $in_mask THEN "
-SPATIAL_FILTER_END = " ELSE '' END"
+SPATIAL_FILTER = "$in_mask"
 
 def has_mask_filter( layer ):
     # check if a layer has already a mask filter enabled
@@ -9,20 +8,19 @@ def has_mask_filter( layer ):
     pal.readFromLayer( layer )
     if not pal.enabled:
         return False
-    return pal.fieldName.find(SPATIAL_FILTER_BEGIN) == 0
+    show_expr = pal.dataDefinedProperties.get( QgsPalLayerSettings.Show )
+    if show_expr is None:
+        return False
+    return show_expr.expressionString() == SPATIAL_FILTER
 
 def remove_mask_filter( pal ):
     npal = QgsPalLayerSettings( pal )
-    if npal.enabled and npal.fieldName.find(SPATIAL_FILTER_BEGIN) == 0:
-        l = len(npal.fieldName)
-        l1 = len(SPATIAL_FILTER_BEGIN)
-        l2 = len(SPATIAL_FILTER_END)
-        npal.fieldName = npal.fieldName[l1:l-l2]
+    if npal.enabled and npal.dataDefinedProperties.get( QgsPalLayerSettings.Show ) is not None and \
+            npal.dataDefinedProperties[ QgsPalLayerSettings.Show ].expressionString() == SPATIAL_FILTER:
+        npal.dataDefinedProperties = dict([(k,v) for k,v in npal.dataDefinedProperties.iteritems() if k != QgsPalLayerSettings.Show] )
     return npal
 
 def add_mask_filter( pal ):
     npal = QgsPalLayerSettings( pal )
-    npal.fieldName = SPATIAL_FILTER_BEGIN + npal.fieldName + SPATIAL_FILTER_END
-    npal.isExpression = True
+    npal.setDataDefinedProperty( QgsPalLayerSettings.Show, True, True, SPATIAL_FILTER, '' )
     return npal
-
