300d7229b51b261e867960834196548918025b1a
Xavier Culos <xavier.culos@eau-adour-garonne.fr>
2017-03-17 15:52:47 +0100

    Migration to QGis 3 - first pass on mask_filter.py

--- mask/mask_filter.py
+++ mask/mask_filter.py
@@ -1,18 +1,21 @@
-from qgis.core import *
+from qgis.core import (QgsVectorLayer, QgsPalLayerSettings)
 
 SPATIAL_FILTER = "in_mask"
 
 def has_mask_filter( layer ):
     if not isinstance(layer, QgsVectorLayer):
         return False
+        
     # check if a layer has already a mask filter enabled
     pal = QgsPalLayerSettings()
     pal.readFromLayer( layer )
     if not pal.enabled:
         return False
+        
     show_expr = pal.dataDefinedProperties.get( QgsPalLayerSettings.Show )
     if show_expr is None:
         return False
+        
     return show_expr.expressionString().startswith(SPATIAL_FILTER)
 
 def remove_mask_filter( pal ):
@@ -20,10 +23,12 @@ def remove_mask_filter( pal ):
     if npal.enabled and npal.dataDefinedProperties.get( QgsPalLayerSettings.Show ) is not None and \
             npal.dataDefinedProperties[ QgsPalLayerSettings.Show ].expressionString().startswith(SPATIAL_FILTER):
         npal.dataDefinedProperties = dict([(k,v) for k,v in npal.dataDefinedProperties.items() if k != QgsPalLayerSettings.Show] )
+        
     return npal
 
 def add_mask_filter( pal, layer ):
     npal = QgsPalLayerSettings( pal )
     expr = "%s(%d)" % (SPATIAL_FILTER, layer.crs().postgisSrid())
     npal.setDataDefinedProperty( QgsPalLayerSettings.Show, True, True, expr, '' )
+    
     return npal
