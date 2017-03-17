83a3468ccaf77a37345c910947b7800a4736969b
Xavier Culos <xavier.culos@eau-adour-garonne.fr>
2017-03-17 15:55:12 +0100

    Migration to QGis 3 - first pass on htmldialog.py

--- mask/htmldialog.py
+++ mask/htmldialog.py
@@ -1,6 +1,5 @@
-from PyQt4.QtCore import * 
-from PyQt4.QtGui import *
-from PyQt4.QtXml import *
+from PyQt5.QtCore import (QSettings, QUrl, Qt) 
+from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QTextBrowser, QDialogButtonBox)
 
 import os
 
